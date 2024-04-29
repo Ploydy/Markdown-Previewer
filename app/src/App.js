6975481d97d36d572dd555c42fe1957e79dd5a57
Ploydy <Lfecarballo20@gmail.com>
2024-04-29 16:03:48 +0800

    finish

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-/* 
-import Editor from './components/editor';
-import Previewer from './components/previewer';
- */
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
 function App() {
+  const initialValue = (
+    '## 2nd value',
+    '# Markdown Preview'
+  )
+  const [ markdown, setMarkdown ] = useState(initialValue);
   return (
-    <main className="App">
-      
+    <main>
+      <section className="markdown">
+        <textarea
+          id='editor'
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article id="preview" className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
     </main>
   );
 }
