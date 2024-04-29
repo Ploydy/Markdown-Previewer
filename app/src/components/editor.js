99fe3de72ee69c6dd0d00d809e37c033bab6357e
Ploydy <Lfecarballo20@gmail.com>
2024-04-29 14:05:45 +0800

    Initial start

---
+++ app/src/components/editor.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Editor() {
+  return (
+    <div className='editor'>
+      <header className='header'>
+        <h3>Editor Page</h3>
+        X icon / Maximize icon
+      </header>
+      <div className='content'>
+        Welcome to my React Markdown Previewer!
+      </div>
+    </div>
+  )
+}
---
+++ app/src/components/previewer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Previewer() {
+  return (
+    <div className='previewer'>
+      <header>
+        <h3> Previewer Page </h3>
+        X icon / Maximize icon
+      </header>
+      <div className=''>
+        <h1> Welcome to my React Markdown Previewer! </h1>
+      </div>
+    </div>
+  )
+}
+
